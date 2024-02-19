9041b39847e97009ed921a777b29fdf1861cc27f
Gitic <146997252+giticnetwork@users.noreply.github.com>
2024-02-19 13:40:39 +0100

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,85 @@
- function openNav() {
-  document.getElementById("mySidepanel").style.width = "100%";
-}
+// ---*** Typing Effects ***---
+var TxtType = function (el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 15) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
 
-function closeNav() {
-  document.getElementById("mySidepanel").style.width = "0";
-}
- 
- 
+TxtType.prototype.tick = function () {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
+
+    setTimeout(function () {
+        that.tick();
+    }, delta);
+};
+
+window.onload = function () {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i = 0; i < elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap {border-right: 2px solid var(--gitic-viocolor)}";
+    document.body.appendChild(css);
+};
+
+// Distribution Container Code====
+setTimeout(function start() {
+
+    $('.bar').each(function (i) {
+        var $bar = $(this);
+        $(this).append('<span class="count"></span>')
+        setTimeout(function () {
+            $bar.css('width', $bar.attr('data-percent'));
+        }, i * 100);
+    });
+
+    $('.count').each(function () {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).parent('.bar').attr('data-percent')
+        }, {
+            duration: 2000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now) + '%');
+            }
+        });
+    });
+
+}, 500)
 
 
 
@@ -14,61 +87,50 @@ function closeNav() {
 
 
 
+// script.js
+// Set the date we're counting down to
+var countDownDate = new Date("Feb 22, 2024 15:37:25").getTime();
 
+// Update the count down every 1 second
+var x = setInterval(function () {
 
+    // Get today's date and time
+    var now = new Date().getTime();
 
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
 
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+    // Output the result in an element with id="demo"
+    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
 
-
-(function () {
-  const second = 1000,
-    minute = second * 60,
-    hour = minute * 60,
-    day = hour * 24;
-
-  //I'm adding this section so I don't have to keep updating this pen every year :-)
-  //remove this if you don't need it
-  let today = new Date(),
-    dd = String(today.getDate()).padStart(2, "0"),
-    mm = String(today.getMonth() + 1).padStart(2, "0"),
-    yyyy = today.getFullYear(),
-    nextYear = yyyy + 1,
-    dayMonth = "10/18/",
-    birthday = dayMonth + yyyy;
-
-  today = mm + "/" + dd + "/" + yyyy;
-  if (today > birthday) {
-    birthday = dayMonth + nextYear;
-  }
-  //end
-
-  const countDown = new Date(birthday).getTime(),
-    x = setInterval(function () {
-      const now = new Date().getTime(),
-        distance = countDown - now;
-
-      (document.getElementById("days").innerText = Math.floor(distance / day)),
-        (document.getElementById("hours").innerText = Math.floor(
-          (distance % day) / hour
-        )),
-        (document.getElementById("minutes").innerText = Math.floor(
-          (distance % hour) / minute
-        )),
-        (document.getElementById("seconds").innerText = Math.floor(
-          (distance % minute) / second
-        ));
-
-      //do something later when date is reached
-      if (distance < 0) {
-        document.getElementById("headline").innerText = "It's my birthday!";
-        document.getElementById("countdown").style.display = "none";
-        document.getElementById("content").style.display = "block";
+    // If the count down is over, write some text 
+    if (distance < 0) {
         clearInterval(x);
-      }
-      //seconds
-    }, 0);
-})();
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
+
+
+
+
+
+
+
+function vote(system) {
+    // You can implement the logic here to record the vote
+    console.log(`Voted for ${system} system`);
+    alert(`Vote ${system === 'token' ? 'Token System' : 'Tokenless System'} on 22 Feb  Your Vote is Your Voice`);
+}
+
+
+
 
 
 
